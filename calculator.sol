083b76db4068bae6f86916e18ecdcb936d9de524
jonboribbins <143540988+jonboribbins@users.noreply.github.com>
2023-09-10 23:04:12 -0400

    Create calculator.sol
    solidity file

---
+++ calculator.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Calculator {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        require(a >= b, "Subtraction result would be negative.");
+        return a - b;
+    }
+}
